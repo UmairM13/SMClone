5bfbcbfd7f8e1aebb8f73a9bc4660e9f050b0124
UmairM13 <22493168@stu.mmu.ac.uk>
2024-09-28 16:02:55 +0100

    post not found exception

---
+++ SpringFSD/src/main/java/com/example/springfsd/app/exception/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.springfsd.app.exception;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
