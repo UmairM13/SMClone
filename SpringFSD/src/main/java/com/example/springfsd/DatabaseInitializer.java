a7379583ba756c011e64b92cb391875fc0bbf40e
UmairM13 <22493168@stu.mmu.ac.uk>
2024-09-28 18:36:45 +0100

    Included a unique id in likes and followers instead of a composite key

--- SpringFSD/src/main/java/com/example/springfsd/DatabaseInitializer.java
+++ SpringFSD/src/main/java/com/example/springfsd/DatabaseInitializer.java
@@ -32,16 +32,16 @@ public class DatabaseInitializer {
                 "FOREIGN KEY(author_id) REFERENCES users(id))");
 
         jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS likes (" +
+                "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
                 "post_id BIGINT," +
                 "user_id BIGINT," +
-                "PRIMARY KEY(post_id, user_id)," +
                 "FOREIGN KEY(post_id) REFERENCES posts(post_id)," +
                 "FOREIGN KEY(user_id) REFERENCES users(id))");
 
         jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS followers (" +
-                "user_id BIGINT," +
+                "id BIGINT AUTO_INCREMENT PRIMARY KEY," +
                 "follower_id BIGINT," +
-                "PRIMARY KEY (user_id, follower_id)," +
+                "user_id BIGINT," +
                 "FOREIGN KEY (user_id) REFERENCES users (id)," +
                 "FOREIGN KEY (follower_id) REFERENCES users(id))");
     }
