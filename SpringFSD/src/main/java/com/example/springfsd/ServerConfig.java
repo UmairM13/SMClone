8d602166a29e77a8d78d33f559726e715927ac5f
UmairM13 <22493168@stu.mmu.ac.uk>
2024-09-28 20:13:22 +0100

    included patch

--- SpringFSD/src/main/java/com/example/springfsd/ServerConfig.java
+++ SpringFSD/src/main/java/com/example/springfsd/ServerConfig.java
@@ -13,7 +13,7 @@ public class ServerConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 
     @Bean
